97ae7570b317ad40de4a18b5b2fc00f0a988dbca
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-12-11 16:55:34 +0400

    Functions from answers

--- chapter11/math/math.go
+++ chapter11/math/math.go
@@ -8,3 +8,25 @@ func Average(xs []float64) float64 {
 	}
 	return total / float64(len(xs))
 }
+
+// Max Returns the maximum value from the array
+func Max(xs []float64) float64 {
+	max := xs[0]
+	for _, val := range xs[1:] {
+		if val > max {
+			max := val
+		}
+	}
+	return max
+}
+
+// Min Returns the minimum value from the array
+func Min(xs []float64) float64 {
+	min := xs[0]
+	for _, val := range xs[1:] {
+		if val < min {
+			min := val
+		}
+	}
+	return min
+}
