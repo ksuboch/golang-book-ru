8dbea1185bbed144c51128923e3aeacf2acc5747
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-12-12 12:17:56 +0400

    Writing + Reading

--- chapter13/files/main.go
+++ chapter13/files/main.go
@@ -4,22 +4,31 @@ import "os"
 import "fmt"
 
 func main() {
-	file, err := os.Open("test.txt")
+	fileW, err := os.Create("test.txt")
+	if err != nil {
+		// handle the error here
+		return
+	}
+	defer fileW.Close()
+
+	fileW.WriteString("test")
+
+	fileR, err := os.Open("test.txt")
 	if err != nil {
 		// handle the error
 		return
 	}
-	defer file.Close()
+	defer fileR.Close()
 
 	// get the file size
-	stat, err := file.Stat()
+	stat, err := fileR.Stat()
 	if err != nil {
 		return
 	}
 
 	// read the file
 	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
+	_, err = fileR.Read(bs)
 	if err != nil {
 		return
 	}
